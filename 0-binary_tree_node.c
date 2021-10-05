687ff2d485b09241c39b5d45a8862fe0666e399c
Adam Taylor <tayloradam1999@gmail.com>
2021-10-05 18:18:52 +0000

    Pushing required file for task 0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Creates a binary tree node
+ * @parent: Pointer to parent node
+ * @value: Value to give to node
+ * Return: Pointer to new node or NULL on failure
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *tmp = NULL;
+	tmp = malloc(sizeof(binary_tree_t));
+	
+	if (!tmp)
+		return (NULL);
+
+	tmp->parent = parent;
+	tmp->n = value;
+	return (tmp);
+}
