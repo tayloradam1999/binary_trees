aa375882a117470eef78f8ff310797189528662f
Adam Taylor <tayloradam1999@gmail.com>
2021-10-05 18:37:32 +0000

    Pushing required file for task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - Insert node as left child of another node
+ * @parent: pointer to parent node
+ * @value: value to give new node
+ * Return: Pointer to new node, NULL if failure or if parent is NULL
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	if (!parent)
+		return (NULL);
+
+	new_node = binary_tree_node(parent, value);
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->left = parent->left;
+
+	if (new_node->left)
+		new_node->left->parent = new_node;
+
+	parent->left = new_node;
+}
