2b97a062d45fd9d59c2e40f27a43576e731aac3e
Adam Taylor <tayloradam1999@gmail.com>
2021-10-10 00:30:46 +0000

    Pushing file for task 11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - calculates size of binary tree
+ * @tree: pointer to root node
+ * Return: If tree is null return 0, else return size
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+}
