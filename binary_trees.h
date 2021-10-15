c8fe78f53814809652ca621b83a5b782243a6521
Adam Taylor <tayloradam1999@gmail.com>
2021-10-15 16:18:53 +0000

    Pushing updated header

--- binary_trees.h
+++ binary_trees.h
@@ -34,7 +34,7 @@ int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
-size_t binary_tree_height(const binary_tree_t *tree);
+int binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
@@ -44,5 +44,6 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+int is_perfect(const binary_tree_t *tree, int d, int level);
 
 #endif /* _BINARY_TREES_H_ */
